2ea670ee2577f22747d753e025466c3ecf301f65
Colby04 <cjcodes2024@gmail.com>
2024-11-25 18:24:17 -0600

    Removed all .js files and updated the package.json file to reflect the changes made to the project.

--- Develop/server/src/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-var User_1 = require("./User");
-exports.User = User_1.default;
-exports.default = { User: User_1.default };
